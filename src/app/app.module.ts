933a1a1d842cd86932c6864f69858b17bb8e1b8c
Agilay Dones <agilaydones@gmail.com>
2019-04-01 21:21:33 -0400

    Work

---
+++ src/app/app.module.ts
@@ -0,0 +1,43 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { NgModel ,FormsModule} from '@angular/forms'; 
+
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TrackComponentComponent } from '../app/components/track-component/track-component.component';
+import { NewplaylistComponent } from './components/newplaylist/newplaylist.component';
+import { AddTrackComponent } from '../app/components/add-track/add-track.component';
+import { Router, Routes, RouterModule } from '@angular/router';
+
+
+const appRoutes: Routes = [
+  {path: 'https://accounts.spotify.com/authorize?client_id=b72324b7926347dc83e63ae5d04366f4&response_type=code&redirect_uri=http://localhost:4200/callback&state=sdsfca&scope=user-read-private%20user-read-email', 
+  component: NewplaylistComponent,
+  data: {header: 'accessToken'}
+}
+]
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TrackComponentComponent,
+    NewplaylistComponent,
+    AddTrackComponent,
+    
+    
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes,{enableTracing: true}),
+    FormsModule
+   
+    
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
